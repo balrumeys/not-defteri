a4edd1101ce81dc24a56cf7ad2639f477baee45b
balrumeys <balrumeyst@gmail.com>
2025-07-07 11:32:42 +0300

    (feat) app :  bootstrap

--- home.php
+++ home.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <title>PHP Not Defteri</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/css/bootstrap.min.css" integrity="sha512-fw7f+TcMjTb7bpbLJZlP8g2Y4XcCyFZW8uy8HsRZsH/SwbMw0plKHFHr99DN3l04VsYNwvzicUX/6qurvIxbxw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="style.css">
 </head>
 
@@ -11,8 +13,8 @@
     <h1>Not Defteri</h1>
 
     <form method="POST">
-        <input type="text" name="title" placeholder="Not Başlığı" required><br><br>
-        <textarea name="content" placeholder="Not İçeriği" required></textarea><br><br>
+        <input type="text" name="title" placeholder="Başlığı giriniz" required><br><br>
+        <textarea name="content" placeholder="Notunuzu yazınız" required></textarea><br><br>
         <button type="submit">Kaydet</button>
     </form>
 
@@ -29,6 +31,7 @@
     <?php else: ?>
         <p>Henüz not eklenmedi.</p>
     <?php endif; ?>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/js/bootstrap.min.js" integrity="sha512-zKeerWHHuP3ar7kX2WKBSENzb+GJytFSBL6HrR2nPSR1kOX1qjm+oHooQtbDpDBSITgyl7QXZApvDfDWvKjkUw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 
 </html>
