fd8d606b081dc7e0b6655816ef25adb74c775268
balrumeys <balrumeyst@gmail.com>
2025-07-08 17:34:41 +0300

    (feat) app: edit note

--- edit.php
+++ edit.php
@@ -1,3 +1,38 @@
+<?php
+
+$id = $_GET['id'] ?? null;
+
+$notes = [];
+if (file_exists('notes.json')) {
+    $notes = json_decode(file_get_contents('notes.json'), true);
+}
+$note = $notes[$id] ?? null;
+if (empty($note)) {
+    http_response_code(404);
+    exit;
+}
+
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $title = trim($_POST['title']);
+    $content = trim($_POST['content']);
+
+    if ($title !== '' && $content !== '') {
+        $newNote = [
+            'title' => htmlspecialchars($title),
+            'content' => htmlspecialchars($content),
+            'date' => date("Y-m-d H:i:s"),
+        ];
+
+        $notes[$id] = $newNote;
+        file_put_contents('notes.json', json_encode($notes, JSON_PRETTY_PRINT));
+    }
+
+    header("Location: index.php");
+    exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="tr">
 
@@ -14,15 +49,15 @@
 
         <form method="POST">
             <label for="title" class="form-label">Başlık:</label>
-            <input name="title" type="text">
+            <input name="title" type="text" value="<?php echo htmlspecialchars($note['title']); ?>" class="form-control" required>
 
             <label for="content" class="form-label">Not:</label>
-            <textarea name="content"></textarea>
+            <textarea name="content" placeholder="Content"><?php echo htmlspecialchars($note['content']); ?></textarea>
 
             <button type="submit" class="btn btn-primary">Kaydet</button>
 
         </form>
-        
+
         <a class="btn btn-primary mt-3" href="index.php">Geri Dön</a>
 
     </div>
