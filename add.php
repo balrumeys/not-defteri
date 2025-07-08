aa9339f2fd15b1b73fa2a3e0b1d9367e36604a02
balrumeys <balrumeyst@gmail.com>
2025-07-08 10:54:01 +0300

    (fix) add.php: better id generation

--- add.php
+++ add.php
@@ -11,11 +11,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             'date' => date("Y-m-d H:i:s"),
         ];
 
-        $notes=[];
+        $notes = [];
         if (file_exists('notes.json')) {
             $notes = json_decode(file_get_contents('notes.json'), true);
         }
-        $newId = count($notes) + 1; 
+        $newId = array_key_last($notes) + 1;
 
         $notes[$newId] = $newNote;
         file_put_contents('notes.json', json_encode($notes, JSON_PRETTY_PRINT));
