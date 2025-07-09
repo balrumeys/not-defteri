3d6918c3ff85d8530510914a8ab86c06f838d739
balrumeys <balrumeyst@gmail.com>
2025-07-09 16:54:04 +0300

    (UI) notes.php: refactor notes display structure and improve layout

--- notes.php
+++ notes.php
@@ -24,9 +24,35 @@
         </div>
     </header>
 
-<a href="add.php" class="btn btn-success mt-3">Not Ekle</a>
+    <div class="container notes-wrapper">
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/js/bootstrap.min.js" integrity="sha512-zKeerWHHuP3ar7kX2WKBSENzb+GJytFSBL6HrR2nPSR1kOX1qjm+oHooQtbDpDBSITgyl7QXZApvDfDWvKjkUw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <div class="notes vstack gap-3">
+            <?php if (!empty($notes)): ?>
+                <?php foreach ($notes as $id => $note): ?>
+                    <div class="card ">
+                        <div class="card-header d-flex mb-3 justify-content-between">
+                            <div>
+                                <?= $note['title'] ?>
+                                (<?= $note['date'] ?>)
+                            </div>
+                            <div>
+                                <a href="delete.php?id=<?= $id ?>" class="btn btn-danger">Sil</a>
+                                <a href="edit.php?id=<?= $id ?>" class="btn btn-success">Düzenle</a>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <?= nl2br($note['content']) ?>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+        </div>
+    <?php else: ?>
+        <p>Henüz not eklenmedi.</p>
+    <?php endif; ?>
+
+
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/js/bootstrap.min.js" integrity="sha512-zKeerWHHuP3ar7kX2WKBSENzb+GJytFSBL6HrR2nPSR1kOX1qjm+oHooQtbDpDBSITgyl7QXZApvDfDWvKjkUw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 </body>
 
