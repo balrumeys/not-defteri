6c41ad32058ce359caae52adf41640a3294536f2
balrumeys <balrumeyst@gmail.com>
2025-07-10 10:29:46 +0300

    (feat) app: add modular header with dynamic title support

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <header>
+        <div class="full-header">
+            <div class="container">
+                <div class="menu">
+                    <h1><?= $sayfaBaslıgı ?></h1>
+                    <a href="add.php" class="add-note-image-btn">
+                        <img src="./img/add-file.png" alt="Not Ekle" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    </header>
+
+</body>
+
+</html>
