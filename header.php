115b8cd9762d7c3122312899b043ffe94ec3fce2
balrumeys <balrumeyst@gmail.com>
2025-07-11 16:50:42 +0300

    (fix) header.php: clean up

--- header.php
+++ header.php
@@ -1,28 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
+<header>
+    <div class="full-header">
+        <div class="container">
+            <div class="menu">
+                <h1><?= $sayfaBaslıgı ?></h1>
 
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
-
-</head>
-
-<body>
-    <header>
-        <div class="full-header">
-            <div class="container">
-                <div class="menu">
-                    <h1><?= $sayfaBaslıgı ?></h1>
-
-                    <a href="<?= $sayfaLinki ?>" class="add-note-image-btn <?= $id ?>" title="<?= $tooltip ?>" data-bs-toggle="tooltip">
-                        <img src="<?= $sayfaIconu ?>" />
-                    </a>
-                </div>
+                <a href="<?= $sayfaLinki ?>" class="add-note-image-btn <?= $id ?>" title="<?= $tooltip ?>" data-bs-toggle="tooltip">
+                    <img src="<?= $sayfaIconu ?>" />
+                </a>
             </div>
         </div>
-    </header>
-
-</body>
-
-</html>
+    </div>
+</header>
