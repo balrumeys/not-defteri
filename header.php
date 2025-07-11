a9044af81715638a722dec4fe038519d36c55923
balrumeys <balrumeyst@gmail.com>
2025-07-11 16:18:51 +0300

    (refactor) header.php

--- header.php
+++ header.php
@@ -15,8 +15,8 @@
                 <div class="menu">
                     <h1><?= $sayfaBaslıgı ?></h1>
 
-                    <a href="<?= $sayfaLinki ?>" class="add-note-image-btn">
-                        <img src="<?= $sayfaIconu ?>" alt="Not Ekle" />
+                    <a href="<?= $sayfaLinki ?>" class="add-note-image-btn <?= $id ?>" title="<?= $tooltip ?>" data-bs-toggle="tooltip">
+                        <img src="<?= $sayfaIconu ?>" />
                     </a>
                 </div>
             </div>
