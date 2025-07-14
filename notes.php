0a5151b33bd565515f1ef35446bbaa0565d832f3
balrumeys <balrumeyst@gmail.com>
2025-07-14 14:50:57 +0300

    (refactor) notes.php: add templates/notes.php

--- notes.php
+++ notes.php
@@ -1,70 +1,19 @@
 <?php
 $notes = [];
 
-
 if (file_exists('notes.json')) {
     $notes = json_decode(file_get_contents('notes.json'), true);
 }
-?>
 
-<!DOCTYPE html>
-<html>
-
-
-<?php
 $title = "NOTLAR";
-include "head.php";
-?>
+$sayfaBaslıgı = "NOTLAR";
+$islemler = [
+    [
+        "href" => "add.php",
+        "icon" => "img/add-file.png",
+        "title" => "Not Ekle",
+        "class" => "add-note-image-btn",
+    ],
+];
 
-
-<body>
-
-    <?php
-    $sayfaBaslıgı = "NOTLAR";
-    $islemler = [
-        [
-            "href" => "add.php",
-            "icon" => "img/add-file.png",
-            "title" => "Not Ekle",
-            "class" => "add-note-image-btn",
-        ],
-    ];
-    include "header.php";
-    ?>
-
-    <div class="container notes-wrapper">
-
-        <div class="notes vstack gap-3">
-            <?php if (!empty($notes)): ?>
-                <?php foreach ($notes as $id => $note): ?>
-                    <div class="card ">
-                        <div class="card-header d-flex justify-content-between">
-                            <div>
-                                <?= $note['title'] ?>
-                                (<?= $note['date'] ?>)
-                            </div>
-                            <div>
-                                <a href="delete.php?id=<?= $id ?>" class="btn btn-danger">Sil</a>
-                                <a href="edit.php?id=<?= $id ?>" class="btn btn-success">Düzenle</a>
-                            </div>
-                        </div>
-                        <div class="card-body">
-                            <?= nl2br($note['content']) ?>
-                        </div>
-                    </div>
-                <?php endforeach; ?>
-        </div>
-    <?php else: ?>
-        <p>Henüz not eklenmedi.</p>
-    <?php endif; ?>
-
-
-    </div>
-
-    <?php
-    include "footer.php";
-    ?>
-
-</body>
-
-</html>
+include "templates/notes.php";
