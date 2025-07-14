946d9b9e7894e4f5471a4199105490c851c9fdb3
balrumeys <balrumeyst@gmail.com>
2025-07-14 16:12:02 +0300

    (chore) delete.php: format

--- pages/delete.php
+++ pages/delete.php
@@ -7,13 +7,6 @@ if (file_exists('notes.json')) {
     $notes = json_decode(file_get_contents('notes.json'), true);
 }
 
-
-
-
-
-
-
-
 if (isset($notes[$id])) {
     unset($notes[$id]);
     file_put_contents('notes.json', json_encode($notes, JSON_PRETTY_PRINT));
