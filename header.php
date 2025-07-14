e172858a62a42cf55c768bd3b32932b2ad34c3f3
balrumeys <balrumeyst@gmail.com>
2025-07-14 12:45:06 +0300

    (style) header.php: clean up

--- header.php
+++ header.php
@@ -3,41 +3,13 @@
         <div class="container">
             <div class="menu">
                 <h1><?= $sayfaBaslıgı ?></h1>
-
-                <!-- <a href="<?= $oncekiSayfaLink ?>" class="add-note-image-btn <?= $id ?>" title="<?= $tooltip ?>" data-bs-toggle="tooltip">
-                    <img src="<?= $sayfaIconu ?>" />
-                </a> -->
-
                 <?php
-                foreach($islemler as $islem){
-                    echo '<a href="'.$islem['href'].'" class="'.$islem['class'].'" title="'.$islem['title'].'" data-bs-toggle="tooltip">';
-                    echo '<img src="'.$islem['icon'].'" />';
+                foreach ($islemler as $islem) {
+                    echo '<a href="' . $islem['href'] . '" class="' . $islem['class'] . '" title="' . $islem['title'] . '" data-bs-toggle="tooltip">';
+                    echo '<img src="' . $islem['icon'] . '" />';
                     echo '</a>';
-
                 }
-                
-
-                
-                
-                
-                
-                
-                
-                
                 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
             </div>
         </div>
     </div>
