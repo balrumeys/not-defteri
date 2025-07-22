7d07123824359a96d49bb44952382df095a34a18
balrumeys <balrumeyst@gmail.com>
2025-07-22 11:58:56 +0300

    (chore) notes.php: adjusted file path

--- pages/notes.php
+++ pages/notes.php
@@ -13,7 +13,7 @@ if (file_exists($jsonFilePath)) {
 $islemler = [
     [
         "href" => "/notes/add",
-        "icon" => "../img/add-file.png",
+        "icon" => "/img/add-file.png",
         "title" => "Not Ekle",
         "class" => "add-note-image-btn",
     ],
