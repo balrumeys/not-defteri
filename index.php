c8aeee22180bc18a16a0a3cc29a47c60b9aa68cf
balrumeys <balrumeyst@gmail.com>
2025-07-22 10:20:44 +0300

    (refactor) app: add routes.php

--- index.php
+++ index.php
@@ -4,8 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/vendor/autoload.php';
@@ -13,9 +12,6 @@ require __DIR__ . '/vendor/autoload.php';
 $app = AppFactory::create();
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
-});
+include __DIR__ . '/routes.php';
 
 $app->run();
