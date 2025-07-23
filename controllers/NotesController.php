3f2c62b81f70195c2a5120885ee84ddbe5c07000
balrumeys <balrumeyst@gmail.com>
2025-07-23 10:12:12 +0300

    (refactor) NotesController::delete()

--- controllers/NotesController.php
+++ controllers/NotesController.php
@@ -149,4 +149,20 @@ class NotesController
         header("Location: /notes");
         exit;
     }
+    public function delete(Request $request, Response $response, $args)
+    {
+        $id = $args['id'] ?? null;
+        $notes = [];
+        $jsonFilePath = 'notes.json';
+        if (file_exists($jsonFilePath)) {
+            $notes = json_decode(file_get_contents($jsonFilePath), true);
+        }
+
+        if (isset($notes[$id])) {
+            unset($notes[$id]);
+            file_put_contents($jsonFilePath, json_encode($notes, JSON_PRETTY_PRINT));
+        }
+        header("Location: /notes");
+        exit;
+    }
 }
--- pages/delete-post.php
+++ pages/delete-post.php
@@ -1,15 +1,2 @@
 <?php
 
-
-$notes = [];
-$jsonFilePath = 'notes.json';
-if (file_exists($jsonFilePath)) {
-    $notes = json_decode(file_get_contents($jsonFilePath), true);
-}
-
-if (isset($notes[$id])) {
-    unset($notes[$id]);
-    file_put_contents($jsonFilePath, json_encode($notes, JSON_PRETTY_PRINT));
-}
-header("Location: /notes");
-exit;
